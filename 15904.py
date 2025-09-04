219f27de23368900296b46835089cc18ffeea153
hhaminlee <hamin6899@naver.com>
2025-09-04 17:48:50 +0900

    15904 UCPC

--- 15904.py
+++ 15904.py
@@ -1,11 +1,13 @@
 words = input()
 
-result = ''
-for ch in words:
-    if ch == 'U' or ch == 'C' or ch == 'P':
-        result += (ch)
+target = "UCPC"  
+target_index = 0 
 
-if 'UCPC' in result:
+for char in words:
+    if target_index < len(target) and char == target[target_index]:
+        target_index += 1 # 일치하면 다음 문자를 찾기 위해 인덱스를 1 증가
+
+if target_index == len(target):
     print("I love UCPC")
 else:
     print("I hate UCPC")
