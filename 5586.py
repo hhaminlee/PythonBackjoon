4459adde605b3c1acc896722cdcc9f1b4bbe0e2b
hhaminlee <hamin6899@naver.com>
2025-10-23 19:14:09 +0900

    5586 JOI와 IOI

---
+++ 5586.py
@@ -0,0 +1,12 @@
+# 백준 5586 JOI와 IOI
+
+name = input()
+count_j = 0
+count_i = 0
+
+for i in range(len(name) - 2):
+    if 'JOI' in name[i:i+3]:
+        count_j += 1
+    if 'IOI' in name[i:i+3]:
+        count_i += 1
+print(f"{count_j}\n{count_i}")
